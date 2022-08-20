6497fa783d857061e9c19f4b5ba8b158c49f84a3
DaromDesarrollos <romanodavid.desarrollos@gmail.com>
2022-08-20 16:07:33 -0300

    actualizacion 5

--- src/main/java/com/portafolio/Darom/Security/Controller/AuthController.java
+++ src/main/java/com/portafolio/Darom/Security/Controller/AuthController.java
@@ -32,6 +32,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/auth")
 @CrossOrigin(origins = "https://frontendap-98a27.web.app")
+
 public class AuthController {
  
     @Autowired
